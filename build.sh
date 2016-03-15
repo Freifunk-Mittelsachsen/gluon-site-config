abe04a4bc82fcc9834fe0de6d5f42addeca3b644
david <dnoelte@gmail.com>
2016-03-15 14:05:18 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -5,27 +5,28 @@
 BRANCH=${2:-"stable"}
 
 rm -r "images"
+rm build.log
 
-for COMMUNITY in doebeln geringswalde hartha leisnig mittweida oschatz ostrau waldheim
+for COMMUNITY in leisnig waldheim doebeln geringswalde hartha mittweida oschatz ostrau
 do
-  rm ~/gluon/site
-  ln -s ~/gluon-site-config/$COMMUNITY ~/gluon/site
-  mkdir -p "images/"$COMMUNITY
+  rm /data1/gluon/site
+  ln -s /data1/gluon-site-config/$COMMUNITY /data1/gluon/site
+  mkdir -p /data1/gluon-site-config/images/$COMMUNITY
 
-  for TARGET in  ar71xx-generic ar71xx-nand mpc85xx-generic x86-generic x86-kvm_guest x86-xen_domu x86-64
+  for TARGET in ar71xx-generic ar71xx-nand mpc85xx-generic x86-generic x86-kvm_guest x86-xen_domu x86-64
   do
-      cd ~/gluon
+      cd /data1/gluon
   		echo "Starting work on target $TARGET for $COMMUNITY" | tee -a build.log
-  		echo -e "\n\n\nmake GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH update" >> build.log
-  		make GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH update >> build.log 2>&1
-  		echo -e "\n\n\nmake GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH clean" >> build.log
-  		make GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH clean >> build.log 2>&1
-  		echo -e "\n\n\nmake GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH -j3" >> build.log
-  		make GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH -j3 >> build.log 2>&1
+  		echo -e "\n\n\nmake GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH update -j6" >> build.log
+  		make GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH update -j6 >> build.log 2>&1
+  		echo -e "\n\n\nmake GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH clean -j6" >> build.log
+  		make GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH clean -j6 >> build.log 2>&1
+  		echo -e "\n\n\nmake GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH -j6" >> build.log
+  		make GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH -j6 >> build.log 2>&1
   		echo -e "\n\n\n============================================================\n\n" >> build.log
   done
 
-  mv ~/gluon/output/images ~/gluon-site-config/images/$COMMUNITY
+  mv /data1/gluon/output/images /data1/gluon-site-config/images/$COMMUNITY
   echo "Compilation complete for $COMMUNITY"
 done
 
