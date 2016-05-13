269fdd6062c54391975b86c6b9838419ac25abf4
noelte <david.noelte@meidel-gruppe.de>
2016-05-13 09:31:06 +0200

    fixed some loops

--- build.sh
+++ build.sh
@@ -10,17 +10,18 @@ rm -r "images"
 rm /data1/gluon/build.log
 
 # clean and update
-for TARGET in $TARGETS
+for TARGET in ${TARGETS[@]}
 do
     cd /data1/gluon
-    echo "Pre-Building: Starting work on target $TARGET" | tee -a build.log
-    echo -e "\n\n\nmake GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH update -j6" >> build.log
-    make GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH update -j6 >> build.log 2>&1
-    echo -e "\n\n\nmake GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH clean -j6" >> build.log
-    make GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH clean -j6 >> build.log 2>&1
     echo -e "\n\n\n============================================================\n\n" >> build.log
+    echo "Pre-Building: Starting work on target $TARGET" | tee -a build.log
+    echo -e "\n\nmake GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH update -j6" >> build.log
+    make GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH update -j6 >> build.log 2>&1
+    echo -e "\n\nmake GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH clean -j6" >> build.log
+    make GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH clean -j6 >> build.log 2>&1
+    echo -e "\n\n------------------------------------------------------------\n\n" >> build.log
 
-    for COMMUNITY in $COMMUNITIES
+    for COMMUNITY in ${COMMUNITIES[@]}
     do
        rm /data1/gluon/site
        ln -s /data1/gluon-site-config/$COMMUNITY /data1/gluon/site
@@ -29,11 +30,12 @@ do
        echo "Starting work on target $TARGET for $COMMUNITY" | tee -a build.log
        echo -e "\n\n\nmake GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH -j6" >> build.log
        make GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH -j6 >> build.log 2>&1
-       echo -e "\n\n\n============================================================\n\n" >> build.log
+       echo -e "\n\n------------------------------------------------------------\n\n" >> build.log
 
+       echo -e "Moving Images for $COMMUNITY ...\n\n" >> build.log
        mv /data1/gluon/output/images/factory/* /data1/gluon-site-config/images/$COMMUNITY/factory
        mv /data1/gluon/output/images/sysupgrade/* /data1/gluon-site-config/images/$COMMUNITY/sysupgrade
-       echo "Compilation complete for $COMMUNITY"
+       echo "Compilation complete for $COMMUNITY\n\n" >> build.log
     done
 done
 
