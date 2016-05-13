d4455267fa8a842f40d83536579cdf79ab29e26f
noelte <david.noelte@meidel-gruppe.de>
2016-05-13 07:52:19 +0200

    updated target array in build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,22 @@
 
 BRANCH=${2:-"stable"}
 COMMUNITIES=(augustusburg colditz doebeln floeha geringswalde grimma grossschirma hartha leisnig mittweida oschatz ostrau rosswein waldheim wermsdorf)
+TARGETS=(ar71xx-generic ar71xx-nand mpc85xx-generic x86-generic x86-kvm_guest x86-xen_domu x86-64)
 
 rm -r "images"
-rm build.log
+rm /data1/gluon/build.log
+
+# clean and update
+for TARGET in $TARGETS
+do
+    cd /data1/gluon
+    echo "Pre-Building: Starting work on target $TARGET" | tee -a build.log
+    echo -e "\n\n\nmake GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH update -j6" >> build.log
+    make GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH update -j6 >> build.log 2>&1
+    echo -e "\n\n\nmake GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH clean -j6" >> build.log
+    make GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH clean -j6 >> build.log 2>&1
+    echo -e "\n\n\n============================================================\n\n" >> build.log
+done
 
 for COMMUNITY in $COMMUNITIES
 do
@@ -14,14 +27,10 @@ do
   ln -s /data1/gluon-site-config/$COMMUNITY /data1/gluon/site
   mkdir -p /data1/gluon-site-config/images/$COMMUNITY
 
-  for TARGET in ar71xx-generic ar71xx-nand mpc85xx-generic x86-generic x86-kvm_guest x86-xen_domu x86-64
+  for TARGET in $TARGETS
   do
       cd /data1/gluon
   		echo "Starting work on target $TARGET for $COMMUNITY" | tee -a build.log
-  		echo -e "\n\n\nmake GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH update -j6" >> build.log
-  		make GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH update -j6 >> build.log 2>&1
-  		echo -e "\n\n\nmake GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH clean -j6" >> build.log
-  		make GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH clean -j6 >> build.log 2>&1
   		echo -e "\n\n\nmake GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH -j6" >> build.log
   		make GLUON_TARGET=$TARGET GLUON_BRANCH=$BRANCH -j6 >> build.log 2>&1
   		echo -e "\n\n\n============================================================\n\n" >> build.log
